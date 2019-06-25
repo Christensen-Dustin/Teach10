96ce9870eff222fae25d9ceaff56022ab8e11623
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-24 21:53:20 -0700

    Teach10 DATABASE update

--- familyhistoryDB.sql
+++ familyhistoryDB.sql
@@ -1,5 +1,5 @@
 CREATE TABLE person (
-    id          SERIAL  PRIMARY KEY NOT NULL,
+    id          SERIAL PRIMARY KEY NOT NULL,
     firstN      VARCHAR(100) NOT NULL,
     lastN       VARCHAR(100), 
     birthday    DATE
@@ -13,20 +13,15 @@ INSERT INTO person (firstN, lastN, birthday) values ('Manuel', 'Luna', '1931-06-
 INSERT INTO person (firstN, lastN, birthday) values ('Helen', 'Schmidt', '1926-11-22');
 INSERT INTO person (firstN, lastN, birthday) values ('Janice', 'Underwood', '1930-2-14');
 
--- giving a user access to the specified table
-CREATE USER familyhistoryuser WITH PASSWORD 'elijah';
--- to login, psql -Ufamilyhistoryuser familyhistory<name of database>
--- psql -U(name of user) (name of database)
+CREATE TABLE parent2child (
+    id          SERIAL PRIMARY KEY NOT NULL,
+    parent_FK   INT references person(id),
+    child_FK    INT references person(id)
+);
 
-
--- This way requires each table to grant specified user access
-GRANT SELECT, INSERT, UPDATE ON person TO familyhistoryuser; -- for now use this one
-
--- Open access to a specified table to a specified user
-GRANT ALL ON person TO familyhistoryuser;
-
--- restricted access to all the ables in the public schema to specified user
-GRANT SELECT, INSERT, UPDATE ON schema:public TO familyhistoryuser;
-
-GRANT USAGE, SELECT ON SEQUENCE person_id_seq TO familyhistoryuser;
-
+INSERT INTO parent2child (parent_FK, child_FK) values (2,1);
+INSERT INTO parent2child (parent_FK, child_FK) values (3,1);
+INSERT INTO parent2child (parent_FK, child_FK) values (4,2);
+INSERT INTO parent2child (parent_FK, child_FK) values (6,2);
+INSERT INTO parent2child (parent_FK, child_FK) values (5,3);
+INSERT INTO parent2child (parent_FK, child_FK) values (7,3);
